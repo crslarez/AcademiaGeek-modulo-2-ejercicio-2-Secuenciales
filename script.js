8c9d5632d47b7800d641ccad2a612d89dbd2b6ba
Carlos Salazar <crslarez@gmail.com>
2021-10-05 18:55:23 -0400

    Finalizado

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let totalVentas = 0;
 let salarioBase = 0;
 let com = 0;
 
+salarioBase = Number(prompt('Ingrese su salario base'));
 v1 = Number(prompt('Ingrese venta 1'));
-v2 = Number(prompt('Ingrese venta 1'));
-v3 = Number(prompt('Ingrese venta 1'));
+v2 = Number(prompt('Ingrese venta 2'));
+v3 = Number(prompt('Ingrese venta 3'));
 
 totalVentas = v1 + v2 + v3;
 
+com = totalVentas * 0.1;
+
+totalPagar = salarioBase + com;
+
+alert('El toral a pagar para el vendedor es: ' + totalPagar);
 console.log(totalVentas);
