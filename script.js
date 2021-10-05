7b311c397ec5fb8a3c8dc25175c4c34f88a6aa36
Carlos Salazar <crslarez@gmail.com>
2021-10-05 18:41:25 -0400

    Cambio de variables de texto a número

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ let totalVentas = 0;
 let salarioBase = 0;
 let com = 0;
 
-v1 = prompt('Ingrese venta 1');
-console.log(v1);
+v1 = Number(prompt('Ingrese venta 1'));
+v2 = Number(prompt('Ingrese venta 1'));
+v3 = Number(prompt('Ingrese venta 1'));
+
+totalVentas = v1 + v2 + v3;
+
+console.log(totalVentas);
